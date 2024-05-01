80f734ca230e63afd3db693e9c49ba77f128fe9d
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-01 08:38:03 +0530

    added styles to issues page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStattusBadge from '@/app/components/IssueStattusBadge';
 import prisma from '@/prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
@@ -17,11 +19,14 @@ const IssueDetailPage =  async({params}:props) => {
         notFound();
     return (
     <div>
-        <p>{issue.title}</p>
-        <p>{issue.description}</p>
-        <p>{issue.status}</p>
-        <p>{issue.createdAt.toDateString()}</p>
-
+        <Heading>{issue.title}</Heading>
+        <Flex className='space-x-3' my="2">
+            <IssueStattusBadge status={issue.status}/>
+            <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card>
+          <p>{issue.description}</p>
+        </Card>  
     </div>
   )
 }
