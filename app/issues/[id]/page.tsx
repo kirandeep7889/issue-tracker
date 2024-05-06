1856012b294e64df08387925000b4ce5d1e855e8
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-06 08:06:02 +0530

    added changes

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,48 +1,31 @@
-import IssueStattusBadge from '@/app/components/IssueStattusBadge';
 import prisma from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Box, Grid } from '@radix-ui/themes';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown'
-import delay from 'delay';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
 
-interface props {
-    params: {id:string}
+interface Props {
+  params: { id: string };
 }
-
-const IssueDetailPage =  async({params}:props) => {
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  })
    
+  if (!issue) notFound();
 
-    const issue=await prisma.issue.findUnique({
-    where: {id:parseInt(params.id)}
-  });
-    
-    if(!issue)
-        notFound();
 
-    await delay(2000);
-
-    return (
-    <Grid columns={{initial: "1",md:"2"}} gap="5">
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex className='space-x-3' my="2">
-            <IssueStattusBadge status={issue.status}/>
-            <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose mt-4'>
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>  
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-         <Button>
-           <Pencil2Icon/>
-           <Link href={`/issues/${issue.id}/edit`}>Edit issue</Link>
-         </Button>
-      </Box>  
+       <EditIssueButton issueId={issue.id} />  
+      </Box>
     </Grid>
-  )
+  );
 }
-
-export default IssueDetailPage
+export default IssueDetailPage;
