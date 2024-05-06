1790e14313bc65a64b4756ec70d966bf60368326
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-06 08:46:38 +0530

    added edit api in ui

--- app/issues/components/IssueForm.tsx
+++ app/issues/components/IssueForm.tsx
@@ -34,6 +34,9 @@ const IssueForm = ({issue}:{issue?:Issue}) => {
      const onSubmit=handleSubmit( async(data)=>{
       try {
           setIsSubmitting(true) 
+          if(issue) 
+            await axios.patch('/api/issues/'+issue.id,data)
+          else
           await axios.post('/api/issues',data);
           router.push('/issues')
       } catch (error) {
@@ -68,7 +71,11 @@ const IssueForm = ({issue}:{issue?:Issue}) => {
       <ErrorMessage>
          {errors.description?.message}
        </ErrorMessage>
-       <Button disabled={isSubmitting}>Submit New Issue{isSubmitting &&<Spinner/>}</Button>
+       <Button disabled={isSubmitting}>
+       {
+        issue ? 'Update Issue' : ' Submit New Issue'
+       }{' '}
+        {isSubmitting &&<Spinner/>}</Button>
 
     </form>
     </div>
