e8227e1df7b9d948d33558259ff5c9bec49d0bfe
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-10 07:46:59 +0530

    added login and logout button

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,10 +5,14 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import classNames from 'classnames';
+import { useSession } from 'next-auth/react'; // Import useSession from next-auth/react
+import { Box } from '@radix-ui/themes';
+
 
 const NavBar = () => {
 
     const currentPath=usePathname();
+    const {status,data:session}=useSession();
    
     const links=[
         {label:'Dashboard', href: '/'},
@@ -21,16 +25,28 @@ const NavBar = () => {
           <Link href="/"><AiFillBug/></Link>
           <ul className='flex space-x-6'>
             {links.map((link)=>
+            <li key={link.href}
+            >
             <Link 
-            key={link.href}
              className={classNames({
                 'text-zinc-900' : link.href===currentPath,
                 'text-zinc-500' : link.href !==currentPath,
                 'hover:text-zinc-800 transition-colors' : true
              })}
               href={link.href}>{link.label}</Link>
+              </li>
              )}
           </ul>
+          <Box>
+            {
+                status==="authenticated" && (
+                <Link href="/api/auth/signout">Signout</Link>
+            )}
+                        {
+                status==="unauthenticated" && (
+                <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
       </nav>
   )
 }
