f358e224c8eb291129903ef864f3c2beb842f10f
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-13 09:01:54 +0530

    setup react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import '@radix-ui/themes/styles.css';
 import './theme-config.css'
 import { Container, Theme, ThemePanel } from '@radix-ui/themes';
 import AuthProvider from './api/auth/Provider';
+import QueryClientProvider from './QueryClientProvider';
 
 
 const inter = Inter({ subsets: ['latin'],
@@ -25,16 +26,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme accentColor="violet" scaling="110%">
-            <NavBar/>
-            <main className='p-5'>
-              <Container>
-                {children}
-              </Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+            <AuthProvider>
+              <Theme accentColor="violet" scaling="110%">
+                <NavBar/>
+                <main className='p-5'>
+                  <Container>
+                    {children}
+                  </Container>
+                </main>
+              </Theme>
+            </AuthProvider>
+        </QueryClientProvider>
 
         </body>
     </html>
