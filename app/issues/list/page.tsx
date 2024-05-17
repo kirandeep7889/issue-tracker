3891fb00f233be0da0c5c5d4ee45d3b95d1bf516
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-17 08:18:34 +0530

    sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -22,11 +22,14 @@ const IssuesPage =  async({
   ]
   const statuses=Object.values(Status);
   const status=statuses.includes(searchParams.status) ? searchParams.status : undefined
+  const orderBy= columns.map(column=>column.value).includes(searchParams.orderBy)
+   ?{ [searchParams.orderBy]: 'asc'} : undefined;
   const issues= await prisma.issue.findMany(
     {
       where: {
-          status: status
-      }
+        status,
+      },
+      orderBy
     }
   );
   return (
