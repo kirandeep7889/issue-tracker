46cf21afeefaf9c7847a02eadf32f2f69bba1994
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-17 07:58:24 +0530

    make columns sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,19 +2,47 @@ import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import { Link,IssueStattusBadge } from '../../components';
 import IssuesActions from "./IssuesActions";
+import NextLink from 'next/link';
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
-const IssuesPage =  async() => {
+const IssuesPage =  async({
+  searchParams
+}: {searchParams: {status:Status,orderBy:keyof Issue};
+}) => {
+  const columns:{
+    label:string,
+    value:keyof Issue
+    className?:string
+  }[]=[
+    {label:'Issue',value:'title'},
+    {label:'Status',value:'status',className:'hidden md:table-cell'},
+    {label:'Created',value:'createdAt',className:'hidden md:table-cell'}
 
-  const issues= await prisma.issue.findMany();
+  ]
+  const statuses=Object.values(Status);
+  const status=statuses.includes(searchParams.status) ? searchParams.status : undefined
+  const issues= await prisma.issue.findMany(
+    {
+      where: {
+          status: status
+      }
+    }
+  );
   return (
     <div>
         <IssuesActions/>
         <Table.Root variant='surface'>
           <Table.Header>
             <Table.Row>
-                <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell className='hidden md:table-cell' >Created</Table.ColumnHeaderCell>
+                {
+                  columns.map(column=><Table.ColumnHeaderCell key={column.value}>
+                       <NextLink href={{
+                        query: {...searchParams,orderBy:column.value}
+                       }}>{column.label}</NextLink>
+                       {column.value===searchParams.orderBy && <ArrowUpIcon className='inline'/>}
+                  </Table.ColumnHeaderCell>)
+                }
             </Table.Row>
           </Table.Header>
           <Table.Body>
