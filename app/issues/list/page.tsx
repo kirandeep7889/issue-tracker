0da8efce5e3a89ef1b4fb9d0b83b82c034c9faf1
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-17 08:27:27 +0530

    fix filtering bugs

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -39,7 +39,7 @@ const IssuesPage =  async({
           <Table.Header>
             <Table.Row>
                 {
-                  columns.map(column=><Table.ColumnHeaderCell key={column.value}>
+                  columns.map(column=><Table.ColumnHeaderCell key={column.value} className={column.className}>
                        <NextLink href={{
                         query: {...searchParams,orderBy:column.value}
                        }}>{column.label}</NextLink>
