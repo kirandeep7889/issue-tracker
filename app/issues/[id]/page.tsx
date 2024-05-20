4788ee2f83163c72c0d38ac412a12ca210f0e539
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-20 08:04:37 +0530

    add meta data

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -40,4 +40,17 @@ const session= await getServerSession(authOptions)
     </Grid>
   );
 }
+
+export async function generateMetaData({ params}: Props) {
+  const issue=await prisma.issue.findUnique({
+    where: {
+      id:parseInt(params.id)
+    }
+  })
+  return {
+    title: issue?.title,
+    description : 'Details of issue' + issue?.id
+  }
+} 
+
 export default IssueDetailPage;
